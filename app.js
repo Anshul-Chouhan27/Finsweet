a145484f13a8cc0337f4bf00bbeb038a40636f91
Anshul-Chouhan27 <anshulchouhan004@gmail.com>
2024-11-15 21:27:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("Home").addEventListener("click",function(){
+    window.location.href="index.html";
+});
+
+document.getElementById("Blog").addEventListener("click",function(){
+    window.location.href="Screen2.html";
+});
+
+document.getElementById("About").addEventListener("click",function(){
+    window.location.href="Screen3.html";
+});
